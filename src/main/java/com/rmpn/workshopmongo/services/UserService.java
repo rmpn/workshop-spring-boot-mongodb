713f9ba0fce67329939455b5051ea2e83751c388
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-18 20:31:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/rmpn/workshopmongo/services/UserService.java
+++ src/main/java/com/rmpn/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rmpn.workshopmongo.domain.User;
+import com.rmpn.workshopmongo.dto.UserDTO;
 import com.rmpn.workshopmongo.repository.UserRepository;
 import com.rmpn.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,25 +16,33 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	
-	
-    public List<User> findAll() {
+
+	public List<User> findAll() {
 		return repo.findAll();
-    	
-    }
-    
-    public User findById(String id) {
-    	
-    	if (!repo.existsById(id)) {
-    		throw new ObjectNotFoundException("Objeto não encontrado");
-    	}
-    	
-    	
-    	
-    	return repo.findById(id).get();
-    	
-    	
+
 	}
+
+	public User findById(String id) {
+
+		if (!repo.existsById(id)) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+
+		return repo.findById(id).get();
+
+	}
+
+	public User insert(User obj) {
+
+		return repo.insert(obj);
+
+	}
+	
+	
+	public User fromDTO(UserDTO objDTO) {
+		
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
+	
 
 }
