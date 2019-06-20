cacad1131b3c5ace062175c54b512930a05466eb
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-20 17:46:54 -0300

    Endpint para retornar os posts de um usuarios

--- src/main/java/com/rmpn/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rmpn/workshopmongo/resources/UserResource.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rmpn.workshopmongo.domain.Post;
 import com.rmpn.workshopmongo.domain.User;
 import com.rmpn.workshopmongo.dto.UserDTO;
 import com.rmpn.workshopmongo.services.UserService;
@@ -71,6 +71,15 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id) {
+
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPost());
 
 	}
 
