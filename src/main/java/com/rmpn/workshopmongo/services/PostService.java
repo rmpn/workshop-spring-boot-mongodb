d1170772a94a6e5f0b9ef88fb71e5d337ef62e46
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-21 10:37:06 -0300

    Consulta simple @QUERY

--- src/main/java/com/rmpn/workshopmongo/services/PostService.java
+++ src/main/java/com/rmpn/workshopmongo/services/PostService.java
@@ -34,7 +34,8 @@ public class PostService {
 
 	public List<Post> findByTitle(String text) {
 
-		return repo.findByTitleContainingIgnoreCase(text);
+		//return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 
 	public Post insert(Post obj) {
