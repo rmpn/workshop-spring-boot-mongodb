026fdd0a6cf724a2c98598078c92a3b7e6c3ba47
Raimundo M P Neto <rmpn.pinto@gmail.com>
2019-06-21 17:52:46 -0300

    Primeira Class Position

--- src/main/java/com/rmpn/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rmpn/workshopmongo/repository/PostRepository.java
@@ -19,5 +19,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	@Query("{ $and: [ {date: {$gte: ?1} },{ date: { $lte: ?2} },{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
 	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
+	
+	
 
 }
